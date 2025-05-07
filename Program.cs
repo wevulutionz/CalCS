87242bd7b97db752ca1862b3c3d46f3ae29c75ae
Claudio <Claudiohf20@gmail.com>
2025-05-07 10:23:10 -0500

    se agrega condicional de suma

--- Program.cs
+++ Program.cs
@@ -5,5 +5,5 @@ Console.WriteLine("Ingrese N2");
 int n2= int.Parse(Console.ReadLine());
 Console.WriteLine("Seleccione: Dividir D,Restar R,Multiplicar M, Sumar S");//agregar operacion con la letra 
 string op=Console.ReadLine();
-int res= op=="D"?cal.Dividir(n1,n2):op=="R"?cal.Restar(n1,n2): op == "M" ? cal.Multiplicar(n1, n2):op == "D" ? cal.Dividir(n1, n2):0;//aumentar condicional siempre al final con :0
+int res= op=="D"?cal.Dividir(n1,n2):op=="R"?cal.Restar(n1,n2): op == "M" ? cal.Multiplicar(n1, n2):op == "D" ? cal.Dividir(n1, n2):op == "S" ? cal.Sumar(n1, n2):0;//aumentar condicional siempre al final con :0
 Console.WriteLine("resultado " + res);
