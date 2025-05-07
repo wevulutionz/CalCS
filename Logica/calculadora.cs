4075574fd4ed8c661b6ae0dc8bddd0d2d21199ec
Claudio <Claudiohf20@gmail.com>
2025-05-07 09:10:50 -0500

    se agrega dividar Claudio

---
+++ Logica/calculadora.cs
@@ -0,0 +1,3 @@
+public class Calculadora {
+    public int Dividir(int n1,int n2) => n1 /n2; //cambiar signo
+}
---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿Calculadora cal = new Calculadora();
+Console.WriteLine("Ingrese N1");
+int n1= int.Parse(Console.ReadLine());
+Console.WriteLine("Ingrese N2");
+int n2= int.Parse(Console.ReadLine());
+Console.WriteLine("Seleccione: Dividir D");//agregar operacion con la letra 
+string op=Console.ReadLine();
+int res= op=="D"?cal.Dividir(n1,n2):0;//aumentar condicional siempre al final con :0
+Console.WriteLine("resultado " + res);
