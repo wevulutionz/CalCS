531dd65a28471ad1f593e341abbcebe508cf97b9
steven <stevensaman968@gmail.com>
2025-05-07 09:23:52 -0500

    se agrega la multiplicacion

--- Program.cs
+++ Program.cs
@@ -1,9 +1,9 @@
 ﻿Calculadora cal = new Calculadora();
 Console.WriteLine("Ingrese N1");
-int n1= int.Parse(Console.ReadLine());
+int n1 = int.Parse(Console.ReadLine());
 Console.WriteLine("Ingrese N2");
-int n2= int.Parse(Console.ReadLine());
-Console.WriteLine("Seleccione: Dividir D");//agregar operacion con la letra 
-string op=Console.ReadLine();
-int res= op=="D"?cal.Dividir(n1,n2):0;//aumentar condicional siempre al final con :0
+int n2 = int.Parse(Console.ReadLine());
+Console.WriteLine("Seleccione: Dividir D, Multiplicar M");//agregar operacion con la letra 
+string op = Console.ReadLine();
+int res = op == "D" ? cal.Dividir(n1, n2) : op == "M" ? cal.Multiplicar(n1, n2) : 0;//aumentar condicional siempre al final con :0
 Console.WriteLine("resultado " + res);
